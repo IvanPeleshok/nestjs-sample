bf74a3448f8d445c3730eef50ac9227d08fc2808
Ivan Peleshok <peleshok.ivan@gmail.com>
2022-11-24 17:21:43 +0300

    init

---
+++ src/product/product.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ProductController } from './product.controller';
+import { ProductModel } from './product.model';
+
+@Module({
+    controllers: [ProductController],
+    imports: [
+        TypegooseModule.forFeature([
+            {
+                typegooseClass: ProductModel,
+                schemaOptions: {
+                    collection: 'Auth',
+                }
+            }
+        ])
+    ]
+})
+export class ProductModule {}
